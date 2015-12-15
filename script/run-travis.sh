164dd60b2c768785502048532e862b8af872ff21
Josh Hagins <hagins.josh@gmail.com>
2015-12-15 03:15:44 -0500

    Make travis script more robust

--- script/run-travis.sh
+++ script/run-travis.sh
@@ -1,9 +1,47 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o errtrace
+set -o nounset
 
-for formula in *.rb; do
-  brew audit --strict "$formula"
-  brew install "$formula"
-  brew test "$formula"
-done
+tap='travis-tmp/nodenv-head-only'
+formula=''
+
+main () {
+  tap_repo
+  trap untap_repo EXIT
+  trap uninstall_formula ERR
+  for formula_file in *.rb; do
+    formula="${formula_file%.rb}"
+    install_formula
+    audit_formula
+    test_formula
+    uninstall_formula
+  done
+}
+
+tap_repo () {
+  brew tap --depth=1 "$tap" "file://$PWD"
+}
+
+untap_repo () {
+  brew untap "$tap"
+}
+
+install_formula () {
+  brew install --HEAD "$tap/$formula"
+}
+
+uninstall_formula () {
+  brew uninstall "$tap/$formula"
+}
+
+audit_formula () {
+  brew audit --strict "$tap/$formula"
+}
+
+test_formula () {
+  brew test "$tap/$formula"
+}
+
+main
